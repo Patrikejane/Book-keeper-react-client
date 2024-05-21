aac722bd166d787aaeb1359e8cd6d41b075cad90
Sunimal Malkaklulage <sskmal@gmail.com>
2024-05-22 00:20:46 +0900

    Update book.tsx
    Added Book props

--- src/components/book.tsx
+++ src/components/book.tsx
@@ -1,25 +1,23 @@
-import React from 'react'
+import React from "react";
 
 interface BookProps {
-    name: string;
-    price: number;
-    image: string;
-  }
-
-const Book:React.FC<BookProps> = ({name,price,image}) => {
-  return (
-    <div>
-        <article className='book'>
-
-            <img src={image} alt={name} className='img' />
-            <div>
-                <h4>{name}</h4>
-                <p>{price}</p>
-            </div>
-        </article>
-      
-    </div>
-  )
+  name: string;
+  price: number;
+  image: string;
 }
 
-export default Book
+const Book: React.FC<BookProps> = ({ name, price, image }) => {
+  return (
+    <div>
+      <article className="book">
+        <img src={image} alt={name} className="img" />
+        <div>
+          <h4>{name}</h4>
+          <p>${price}</p>
+        </div>
+      </article>
+    </div>
+  );
+};
+
+export default Book;

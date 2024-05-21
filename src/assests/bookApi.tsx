fcefa5d8a6a79a73a828540bc3a0618047a6035f
Sunimal Malkaklulage <sskmal@gmail.com>
2024-05-22 00:20:32 +0900

    Create bookApi.tsx
    Book Data

---
+++ src/assests/bookApi.tsx
@@ -0,0 +1,36 @@
+const data =  [
+    {
+      id: 1,
+      name: 'Atomic Habits',
+      price: 13.5,
+      image: 'https://images-na.ssl-images-amazon.com/images/I/81YkqyaFVEL._AC_UL600_SR600,400_.jpg',
+    },
+    {
+      id: 2,
+      name: 'The 48 Laws of Power',
+      price: 14.5,
+      image: 'https://images-na.ssl-images-amazon.com/images/I/611X8GI7hpL._AC_UL600_SR600,400_.jpg',
+    },
+    {
+      id: 3,
+      name: 'The Psychology of Money',
+      price: 10.98,
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71aG0m9XRcL._AC_UL600_SR600,400_.jpg',
+    },
+    {
+      id: 4,
+      name: 'OutLive',
+      price: 19.5,
+      image: 'https://images-na.ssl-images-amazon.com/images/I/71X9FMy66NL._AC_UL600_SR600,400_.jpg',
+    },
+    {
+      id: 5,
+      name: 'The Creative Act: A Way of Being',
+      price: 16.0,
+      image: 'https://images-na.ssl-images-amazon.com/images/I/918EkrTDaRL._AC_UL300_SR300,200_.jpg',
+    },
+  ];
+
+
+
+export default data

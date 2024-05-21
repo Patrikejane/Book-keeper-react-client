6cb6bef0f66a4a1412ffe2e151ed99c398ec08bc
Sunimal Malkaklulage <sskmal@gmail.com>
2024-05-22 00:22:13 +0900

    Update App.tsx
    App updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import Booklist from "./components/booklist";
+import data from "./assests/bookApi";
 
-function App() {
+const App = () => {
+  const [bookdata, setBookData] = useState(data);
+
+  console.log(data);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <section className="container">
+        <div className="topic">Bookkeeper </div>
+        <h3> {bookdata.length} books in the list</h3>
+        <Booklist booksProps={bookdata} />
+        <button className="btn btn-block"> Clear All</button>
+      </section>
+    </main>
   );
-}
+};
 
 export default App;
